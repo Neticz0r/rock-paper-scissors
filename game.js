207a3347f7403c85077afad362c9b9d8079465a7
Patrick Rodrigues <rodpa715@outlook.com>
2019-02-23 14:14:46 +0100

    added some functionality to the html and reworked the js

--- game.js
+++ game.js
@@ -10,29 +10,29 @@ function cpuPlay(){
 }
 
 function gameConditions(){
-    cpuPlay();
+
     if (localPlay === cpuChoice){
-        console.log("draw")
+        //console.log("draw")
     // ROCK
     } else if (localPlay === choices[0] && cpuChoice === choices[1]){
-        console.log("Cpu Wins")
+        //console.log("Cpu Wins")
         return scoreCPU = scoreCPU + 1
     } else if (localPlay === choices[0] && cpuChoice === choices[2]){
-        console.log("Player Wins")
+        //console.log("Player Wins")
         return scorePlayer = scorePlayer + 1
     // PAPER
     } else if (localPlay === choices[1] && cpuChoice === choices[0]){
-        console.log("Player Wins")
+        //console.log("Player Wins")
         return scorePlayer = scorePlayer + 1
     } else if (localPlay === choices[1] && cpuChoice === choices[2]){
-        console.log("Cpu Wins")
+        //console.log("Cpu Wins")
         return scoreCPU = scoreCPU + 1
     // SCISSORS
     } else if (localPlay === choices[2] && cpuChoice === choices[0]){
-        console.log("Cpu Wins")
+        //console.log("Cpu Wins")
         return scoreCPU = scoreCPU + 1
     } else if (localPlay === choices[2] && cpuPlay === choices[1]){
-        console.log("Player Wins")
+        //console.log("Player Wins")
         return scorePlayer = scorePlayer+ 1
     // DEBUG
     } else {
@@ -40,13 +40,25 @@ function gameConditions(){
     }
 }
 
-function game(){
-    for (i = 0; i < 5; i++){
-        gameConditions();
-        }   
+
+
+function shoot(){
+    cpuPlay();
+    imageChanger(); 
 }
 
-game();
-console.log("Player score: " + scorePlayer)
-console.log("Cpu score: " + scoreCPU)
 
+function imageChanger(){
+    let cpuImage = document.getElementById("cpuImg")
+    if (cpuChoice === choices[0]){
+        cpuImage.src = "./images/rock.png"
+    } else if (cpuChoice === choices[1]) {
+        cpuImage.src = "./images/paper.png"
+    } else if (cpuChoice === choices[2]) {
+        cpuImage.src = "./images/scissors.png"
+    }
+}
+
+/*
+console.log("Player score: " + scorePlayer)
+console.log("Cpu score: " + scoreCPU) */
